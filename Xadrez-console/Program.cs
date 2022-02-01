97ffc74b3a48380beb569d3256ddfcf89643db0d
Arnaux Stevan <arnauxx@hotmail.com>
2022-02-01 13:18:09 -0300

    Inicio das peças no tabuleiro

--- Xadrez-console/Program.cs
+++ Xadrez-console/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using tabuleiro;
+using Xadrez;
 
 namespace Xadrez_console
 {
@@ -9,6 +10,11 @@ namespace Xadrez_console
         {
 
             Tabuleiro tab = new Tabuleiro(8, 8);
+
+            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+            tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
+
 
             Tela.imprimirTabuleiro(tab);
 
