038142dbea24a72bac99ae5476b5d5bd1aa94b16
Arnaux Stevan <arnauxx@hotmail.com>
2022-02-01 14:57:24 -0300

    Fazendo os primeiros testes de movimentacao

--- Xadrez-console/Program.cs
+++ Xadrez-console/Program.cs
@@ -10,17 +10,23 @@ namespace Xadrez_console
         {
             try
             {
-                Tabuleiro tab = new Tabuleiro(8, 8);
+                PartidadeDeXadrez partida = new PartidadeDeXadrez();
 
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
-                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 7));
-                tab.colocarPeca(new Torre(tab, Cor.Branca), new Posicao(3, 3));
+                while (!partida.terminada)
+                {
+                    Console.Clear();
+                    Tela.imprimirTabuleiro(partida.tab);
+                    Console.WriteLine();
+                    Console.Write("Origem: ");
+                    Posicao origem = Tela.lerPosicaoXadrez().toPosicao();
+                    Console.Write("Destino: ");
+                    Posicao destino = Tela.lerPosicaoXadrez().toPosicao();
 
+                    partida.executaMovimento(origem, destino);
 
+                }
 
-
-                Tela.imprimirTabuleiro(tab);
+                Tela.imprimirTabuleiro(partida.tab);
 
 
             }
--- Xadrez-console/tabuleiro/Peca.cs
+++ Xadrez-console/tabuleiro/Peca.cs
@@ -15,5 +15,10 @@
             this.qteMovimentos = 0;
         }
 
+        public void incrementarQteMovimentos()
+        {
+            qteMovimentos++;
+        }
+
     }
 }
