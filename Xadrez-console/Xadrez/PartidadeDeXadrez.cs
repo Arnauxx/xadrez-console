5c7835676e1cd599949db30d99dd7c863515362e
Arnaux Stevan <arnauxx@hotmail.com>
2022-02-15 20:27:21 -0300

    Jogada especial promocao e fim do projeto

--- Xadrez-console/Xadrez/PartidadeDeXadrez.cs
+++ Xadrez-console/Xadrez/PartidadeDeXadrez.cs
@@ -147,6 +147,21 @@ namespace Xadrez
                 throw new TabuleiroException("Você não pode se colocar em xeque!");
             }
 
+            Peca p = tab.peca(destino);
+
+            // #jogadaespecial promocao
+            if(p is Peao)
+            {
+                if((p.cor == Cor.Branca && destino.linha == 0) || (p.cor == Cor.Preta && destino.linha == 7)){
+                    p = tab.retirarPeca(destino);
+                    pecas.Remove(p);
+                    Peca dama = new Dama(tab, p.cor);
+                    tab.colocarPeca(dama, destino);
+                    pecas.Add(dama);
+                }
+            }
+
+
             if (estaEmXeque(adversaria(jogadorAtual)))
             {
                 xeque = true;
@@ -166,7 +181,6 @@ namespace Xadrez
                 mudaJogador();
             }
 
-            Peca p = tab.peca(destino);
 
             // #jogadaespecial EnPassant
             if(p is Peao && (destino.linha == origem.linha - 2 || destino.linha == origem.linha + 2))
